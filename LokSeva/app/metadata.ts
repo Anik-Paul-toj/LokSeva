f33db3a4f2b0247ec00e2d3d707a06b0a2739446
Anik Paul <trojanik003@gmail.com>
2025-04-27 18:46:14 +0530

    Delete LokSeva directory

--- LokSeva/app/metadata.ts
+++
@@ -1,8 +0,0 @@
-// app/metadata.ts
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  icons: '/logo.png',
-  title: 'LokSeva',
-  description: 'Developed by ERROR503',
-};
